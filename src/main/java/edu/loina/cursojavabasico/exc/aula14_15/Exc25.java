604ca7973dee779a81cf652d09b34098d272f2d1
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-06-12 20:38:18 -0300

    Adicionando novos exercícios

---
+++ src/main/java/edu/loina/cursojavabasico/exc/aula14_15/Exc25.java
@@ -0,0 +1,57 @@
+package edu.loina.cursojavabasico.exc.aula14_15;
+
+import java.util.Scanner;
+
+public class Exc25 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int cont = 0;
+
+        System.out.println("Telefonou para a vítima? (sim/nao) ");
+        String resposta = scanner.next();
+
+        if (resposta.equalsIgnoreCase("sim")) {
+            cont++;
+        }
+
+        System.out.println("Esteve no local do crime? (sim/nao) ");
+        resposta = scanner.next();
+
+        if (resposta.equalsIgnoreCase("sim")) {
+            cont++;
+        }
+
+        System.out.println("Mora perto da vítima? (sim/nao) ");
+        resposta = scanner.next();
+
+        if (resposta.equalsIgnoreCase("sim")) {
+            cont++;
+        }
+
+        System.out.println("Devia para a vítima? (sim/nao) ");
+        resposta = scanner.next();
+
+        if (resposta.equalsIgnoreCase("sim")) {
+            cont++;
+        }
+
+        System.out.println("Já trabalhou com a vítima? (sim/nao) ");
+        resposta = scanner.next();
+
+
+        if (resposta.equalsIgnoreCase("sim")) {
+            cont++;
+        }
+
+        if (cont == 2) {
+            System.out.println("Suspeita");
+        } else if (cont == 3 || cont == 4) {
+            System.out.println("Cúmplice");
+        } else if (cont == 5) {
+            System.out.println("Assassino");
+        } else {
+            System.out.println("Inocente");
+        }
+    }
+}
---
+++ src/main/java/edu/loina/cursojavabasico/exc/aula14_15/Exc26.java
@@ -0,0 +1,30 @@
+package edu.loina.cursojavabasico.exc.aula14_15;
+
+public class Exc26 {
+    public static void main(String[] args) {
+        double precoAlcool = 1.90;
+        double precoGasolina = 2.50;
+        double desconto = 0;
+        double valorPago = 0;
+        double litrosVendidos = 20;
+        String tipoCombustivel = "A";
+
+        if (tipoCombustivel.equalsIgnoreCase("A")) {
+            if (litrosVendidos <= 20) {
+                desconto = 0.03;
+            } else {
+                desconto = 0.05;
+            }
+            valorPago = litrosVendidos * precoAlcool - (litrosVendidos * precoAlcool * desconto);
+        } else if (tipoCombustivel.equalsIgnoreCase("G")) {
+            if (litrosVendidos <= 20) {
+                desconto = 0.04;
+            } else {
+                desconto = 0.06;
+            }
+            valorPago = litrosVendidos * precoGasolina - (litrosVendidos * precoGasolina * desconto);
+        }
+
+        System.out.println("Valor a ser pago: " + valorPago);
+    }
+}
