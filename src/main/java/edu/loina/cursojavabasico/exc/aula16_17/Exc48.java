ec5760672fed177e51206a3a47a8825c82c64f52
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-06-28 10:45:35 -0300

    Finalizando exercícios da aula 16 e 17

---
+++ src/main/java/edu/loina/cursojavabasico/exc/aula16_17/Exc48.java
@@ -0,0 +1,19 @@
+package edu.loina.cursojavabasico.exc.aula16_17;
+
+import java.util.Scanner;
+
+public class Exc48 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro positivo: ");
+        int numero = scanner.nextInt();
+
+        String numeroStr = Integer.toString(numero);
+        String numeroInvertidoStr = new StringBuilder(numeroStr).reverse().toString();
+
+        int numeroInvertido = Integer.parseInt(numeroInvertidoStr);
+
+        System.out.println("Número invertido: " + numeroInvertido);
+    }
+}
