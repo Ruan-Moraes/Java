ffbf6f5059a29374c1292e51c7483df3a2d9e536
Ruan-Moraes <ruanmoraessantosbarbosa@gmail.com>
2024-06-29 10:29:32 -0300

    Adicionando exercícios da aula 19

---
+++ src/main/java/edu/loina/cursojavabasico/exc/aula19/Exc14.java
@@ -0,0 +1,25 @@
+package edu.loina.cursojavabasico.exc.aula19;
+
+public class Exc14 {
+    public static void main(String[] args) {
+        int[] listaA = new int[10];
+
+        for (int i = 0; i < listaA.length; i++) {
+            listaA[i] = (int) Math.round(Math.random() * 100);
+        }
+
+        int somaTotal = 0;
+
+        for (int i = 0; i < listaA.length; i++) {
+            int eImpar = listaA[i] % 2 == 1 ? listaA[i] : 0;
+
+            if (eImpar > 0) {
+                somaTotal += listaA[i];
+            }
+        }
+
+        double media = somaTotal / listaA.length;
+
+        System.out.println("Média: " + media);
+    }
+}
